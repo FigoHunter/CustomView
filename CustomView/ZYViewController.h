6a1781d7efee9b8b898e38fe742c3ed955160719
zhangyuc <zhangyuc@zhangyuctekiMacBook-Pro.local>
2013-05-21 09:35:33 +0800

    Initial Commit

---
+++ CustomView/ZYViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZYViewController.h
+//  CustomView
+//
+//  Created by zhangyuc on 13-5-21.
+//  Copyright (c) 2013年 zhangyuc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYViewController : UIViewController
+
+@end
---
+++ CustomView/ZYViewController.m
@@ -0,0 +1,29 @@
+//
+//  ZYViewController.m
+//  CustomView
+//
+//  Created by zhangyuc on 13-5-21.
+//  Copyright (c) 2013年 zhangyuc. All rights reserved.
+//
+
+#import "ZYViewController.h"
+
+@interface ZYViewController ()
+
+@end
+
+@implementation ZYViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
